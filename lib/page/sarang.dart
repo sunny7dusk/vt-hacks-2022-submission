170aa14bc03712325613d372e15d344f830b550e
Sarang-R-119 <sarangvr@yahoo.com>
2022-02-26 17:31:22 -0500

    Modified the chatbox functionality

---
+++ lib/page/sarang.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:vt_hacks_submission/components/chatbox_comp.dart';
+import 'package:lottie/lottie.dart';
+
+class FullNewsPage extends StatefulWidget {
+  const FullNewsPage({Key? key}) : super(key: key);
+
+  @override
+  State<FullNewsPage> createState() => _FullNewsPageState();
+}
+
+class _FullNewsPageState extends State<FullNewsPage> {
+  String _message = "";
+
+  _EntryBox() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        color: Colors.white,
+        height: 70.0,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: TextField(
+              textCapitalization: TextCapitalization.sentences,
+              decoration:
+                  InputDecoration.collapsed(hintText: "Ask a question ..."),
+            )),
+            IconButton(
+                onPressed: (value) {
+                  _message = value;
+                },
+                iconSize: 25.0,
+                icon: Icon(Icons
+                    .send), //Lottie.network(                    "https://lottiefiles.com/52054-sending-confirmation.json"),
+                color: Colors.blue)
+          ],
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<ChatBox> items =
+        List<ChatBox>.generate(50, (i) => ChatBox(chatText: 'Item $i'));
+    return Scaffold(
+      backgroundColor: const Color(0xFF7A9BEE),
+      appBar: AppBar(
+        title: const Text('info.friend'),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Column(children: <Widget>[
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(45.0),
+                topRight: Radius.circular(45.0),
+              ),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(45.0),
+                topRight: Radius.circular(45.0),
+              ), // Gets clipped
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 45.0),
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return ChatboxComp(items[index], false);
+                  }),
+            ),
+          ),
+        ),
+        _EntryBox()
+      ]),
+    );
+  }
+}
