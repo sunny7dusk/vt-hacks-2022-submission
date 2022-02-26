0e0ae0deeae2b819c9d7d23263a531dec5331b63
sunny7dusk <nat233947@gmail.com>
2022-02-26 14:40:12 -0500

    Fixed sizing issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:vt_hacks_submission/page/full_news_page.dart';
 import 'package:vt_hacks_submission/page/loading_animation_page.dart';
 
 void main() {
@@ -44,60 +46,62 @@ class _MyHomePageState extends State<MyHomePage> {
           if (snap.data == null) {
             return const LoadingAnimationsPage();
           } else {
-            return Scaffold(
-              backgroundColor: const Color(0xFF7A9BEE),
-              appBar: AppBar(
-                title: const Text('info.friend'),
-                centerTitle: true,
-                backgroundColor: Colors.transparent,
-                elevation: 0.0,
-              ),
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Stack(
-                      children: [
-                        Container(
-                          height: MediaQuery.of(ctx).size.height - 70.0,
-                          width: MediaQuery.of(ctx).size.width,
-                          color: Colors.transparent,
-                        ),
-                        Positioned(
-                          top: 20.0,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(45.0),
-                                topRight: Radius.circular(45.0),
-                              ),
-                              color: Colors.white,
-                            ),
-                          ),
-                          height: MediaQuery.of(ctx).size.height,
-                          width: MediaQuery.of(ctx).size.width,
-                        ),
-                        Column(
-                          children: [
-                            Scrollbar(
-                              child: SingleChildScrollView(
-                                child: Column(
-                                  children: [
-                                    for (var i = 0; i < 100; i++)
-                                      Expanded(child: Text(i.toString()))
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
+            return FullNewsPage();
           }
         });
   }
 }
+
+// Scaffold(
+//               backgroundColor: const Color(0xFF7A9BEE),
+//               appBar: AppBar(
+//                 title: const Text('info.friend'),
+//                 centerTitle: true,
+//                 backgroundColor: Colors.transparent,
+//                 elevation: 0.0,
+//               ),
+//               body: Center(
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: <Widget>[
+//                     Stack(
+//                       children: [
+//                         Container(
+//                           height: MediaQuery.of(ctx).size.height - 70.0,
+//                           width: MediaQuery.of(ctx).size.width,
+//                           color: Colors.transparent,
+//                         ),
+//                         Positioned(
+//                           top: 20.0,
+//                           child: Container(
+//                             decoration: const BoxDecoration(
+//                               borderRadius: BorderRadius.only(
+//                                 topLeft: Radius.circular(45.0),
+//                                 topRight: Radius.circular(45.0),
+//                               ),
+//                               color: Colors.white,
+//                             ),
+//                           ),
+//                           height: MediaQuery.of(ctx).size.height,
+//                           width: MediaQuery.of(ctx).size.width,
+//                         ),
+//                         Column(
+//                           children: [
+//                             Scrollbar(
+//                               child: SingleChildScrollView(
+//                                 child: Column(
+//                                   children: [
+//                                     for (var i = 0; i < 100; i++)
+//                                       Expanded(child: Text(i.toString()))
+//                                   ],
+//                                 ),
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                       ],
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             );
